7e9121687c3a54646a66134f441fb32da2ab92aa
neerajnakka <65122791+neerajnakka@users.noreply.github.com>
2024-03-31 22:46:54 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -6,7 +6,7 @@ import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h1 className="my-20 text-center text-4xl">
+      <h1 className=" my-20 text-center text-4xl">
         About <span className="text-neutral-500">Me</span>
       </h1>
       <div className="flex flex-wrap justify-center lg:justify-between">
