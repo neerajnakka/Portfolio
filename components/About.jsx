ef713bf5bf26c85793756ff529fed2c8e2888e08
neerajnakka <65122791+neerajnakka@users.noreply.github.com>
2024-04-01 14:41:54 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -31,24 +31,24 @@ const About = () => {
           className="w-full lg:w-1/2"
         >
           <div className="flex flex-col justify-center">
-            <p className="my-2 max-w-xl py-6 text-center lg:text-left">
+            <p className=" ml-4 my-2 max-w-xl py-6 text-center lg:text-left">
               {ABOUT_TEXT}
             </p>
             <div className="text-center lg:text-left">
-              <h2 className="text-2xl font-bold text-neutral-300">
+              <h2 className="ml-4 text-2xl font-bold text-neutral-300">
                 Contact Details
               </h2>
               <div className="flex flex-col items-center lg:items-start mt-4 text-lg text-neutral-200">
-                <div className="flex items-center mb-4">
-                  <FaMapMarkerAlt className="mr-2" />
+                <div className="flex items-center mb-4 ">
+                  <FaMapMarkerAlt className="mr-2 ml-4" />
                   <p>{CONTACT.address}</p>
                 </div>
                 <div className="flex items-center mb-4">
-                  <FaPhoneAlt className="mr-2" />
+                  <FaPhoneAlt className="mr-2 ml-4" />
                   <p>{CONTACT.phoneNo}</p>
                 </div>
                 <div className="flex items-center">
-                  <FaEnvelope className="mr-2" />
+                  <FaEnvelope className="mr-2 ml-4" />
                   <a
                     href={`mailto:${CONTACT.email}`}
                     className="hover:text-purple-700 transition duration-300 underline"
